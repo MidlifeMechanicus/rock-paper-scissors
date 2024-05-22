d62e575a31755f62d6f46fd43b438c3e426096b7
ElectronDragon <electricdragondevelopment@gmail.com>
2024-05-22 10:08:21 +0100

    Create getMatchResult() and implement DOM manipulation to track score

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const humanScoreDisplay = document.querySelector("#humanScoreDisplay");
 
 const computerScoreDisplay = document.querySelector("#computerScoreDisplay");
 
-const matchResultDisplay = document.querySelector("#matchResultDisplay");
+const gameNumberDisplay = document.querySelector("#gameNumberDisplay");
 
 const rock = document.querySelector("#rock");
 rock.addEventListener("click", () => {
@@ -26,6 +26,12 @@ rock.addEventListener("click", () => {
     computerChoiceDisplay.textContent = computerChoice;
     console.log(playRound(humanChoice, computerChoice));
     resultDisplay.textContent = result;
+
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+    gameNumberDisplay.textContent = gameNumber;
+
+    getMatchResult();
 })
 
 const paper = document.querySelector("#paper");
@@ -36,6 +42,12 @@ paper.addEventListener("click", () => {
     computerChoiceDisplay.textContent = computerChoice;
     console.log(playRound(humanChoice, computerChoice));
     resultDisplay.textContent = result;
+
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+    gameNumberDisplay.textContent = gameNumber;
+
+    getMatchResult();
 })
 
 const scissors = document.querySelector("#scissors");
@@ -46,97 +58,95 @@ scissors.addEventListener("click", () => {
     computerChoiceDisplay.textContent = computerChoice;
     console.log(playRound(humanChoice, computerChoice));
     resultDisplay.textContent = result;
+
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+    gameNumberDisplay.textContent = gameNumber;
+
+    getMatchResult();
 })
-
-
-
-// function playGame() {
-
     
+function getComputerChoice () {
     
-    // function getHumanChoice () {
-    // 
-        // let humanChoice = "";
-    // 
-        // do {
-            // let humanInput = prompt("Please choose rock, paper, or scissors.");
-            // let humanChoice = humanInput.toUpperCase();
-    // 
-            // if (humanChoice != "ROCK" && humanChoice != "PAPER" && humanChoice != "SCISSORS") {
-                // alert("Your choice is not a valid option. Please try again.");
-            // } else {
-            // return humanChoice;
-            // }
-        // } 
-        // while (humanChoice != "ROCK" && humanChoice != "PAPER" && humanChoice != "SCISSORS");
-        // 
-        // return humanChoice;
-    // }
-    
-    function getComputerChoice () {
-        
-        function getRandom() {
-            const minValue = Math.ceil(1);
-            const maxValue = Math.floor(3);
-            return Math.floor(Math.random() * (maxValue - minValue +1) + minValue);
-        }
-        
-        if (getRandom() == 1) {
-            computerChoice = "ROCK";
-        } else if (getRandom() == 2) {
-            computerChoice = "PAPER";
-        } else {
-            // test == 3
-            computerChoice = "SCISSORS";
-        }
-    
-        return computerChoice;
+    function getRandom() {
+        const minValue = Math.ceil(1);
+        const maxValue = Math.floor(3);
+        return Math.floor(Math.random() * (maxValue - minValue +1) + minValue);
     }
     
-    function playRound(humanChoice, computerChoice) {
-        let win = "You win!";
-        let loss = "You lose!";
-        let tie = "It's a tie!";
-    
-        if (humanChoice == "ROCK" && computerChoice == "ROCK") {
-            gameNumber +=1;
-            result = tie;
-        } else if (humanChoice == "ROCK" && computerChoice == "PAPER") {
-            computerScore +=1;
-            gameNumber +=1;
-            result = loss;
-        } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
-            humanScore +=1;
-            gameNumber +=1;
-            result = win;
-        } else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
-            humanScore +=1;
-            gameNumber +=1;
-            result = win;
-        } else if (humanChoice == "PAPER" && computerChoice == "PAPER") {
-            gameNumber +=1;
-            result = tie;
-        } else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
-            computerScore +=1;
-            gameNumber +=1;
-            result = loss;
-        } else if (humanChoice == "SCISSORS" && computerChoice == "ROCK") {
-            computerScore +=1;
-            gameNumber +=1;
-            result = loss;
-        } else if (humanChoice == "SCISSORS" && computerChoice == "PAPER") {
-            humanScore +=1;
-            gameNumber +=1;
-            result = win;
-        } else if (humanChoice == "SCISSORS" && computerChoice == "SCISSORS") {
-            gameNumber +=1;
-            result = tie;
-        } else {
-            result = "?";
-        }
-    
-        return result;
+    if (getRandom() == 1) {
+        computerChoice = "ROCK";
+    } else if (getRandom() == 2) {
+        computerChoice = "PAPER";
+    } else {
+        // test == 3
+        computerChoice = "SCISSORS";
     }
+
+    return computerChoice;
+}
+    
+function playRound(humanChoice, computerChoice) {
+    let win = "You win!";
+    let loss = "You lose!";
+    let tie = "It's a tie!";
+
+    if (humanChoice == "ROCK" && computerChoice == "ROCK") {
+        gameNumber +=1;
+        result = tie;
+    } else if (humanChoice == "ROCK" && computerChoice == "PAPER") {
+        computerScore +=1;
+        gameNumber +=1;
+        result = loss;
+    } else if (humanChoice == "ROCK" && computerChoice == "SCISSORS") {
+        humanScore +=1;
+        gameNumber +=1;
+        result = win;
+    } else if (humanChoice == "PAPER" && computerChoice == "ROCK") {
+        humanScore +=1;
+        gameNumber +=1;
+        result = win;
+    } else if (humanChoice == "PAPER" && computerChoice == "PAPER") {
+        gameNumber +=1;
+        result = tie;
+    } else if (humanChoice == "PAPER" && computerChoice == "SCISSORS") {
+        computerScore +=1;
+        gameNumber +=1;
+        result = loss;
+    } else if (humanChoice == "SCISSORS" && computerChoice == "ROCK") {
+        computerScore +=1;
+        gameNumber +=1;
+        result = loss;
+    } else if (humanChoice == "SCISSORS" && computerChoice == "PAPER") {
+        humanScore +=1;
+        gameNumber +=1;
+        result = win;
+    } else if (humanChoice == "SCISSORS" && computerChoice == "SCISSORS") {
+        gameNumber +=1;
+        result = tie;
+    } else {
+        result = "?";
+    }
+
+    return result;
+}
+
+function getMatchResult() {
+    let currentGameNumber = gameNumber
+    if (currentGameNumber >= 5) {
+        if (humanScore > computerScore) {
+            winner = "You win the match!"
+        } else if (humanScore < computerScore) {
+            winner = "RPS Bot wins the match!"
+        } else {
+            winner = "It's a tied match!"
+        }
+        alert(winner);
+        humanScore = 0;
+        computerScore = 0;
+        gameNumber=0;
+    } else {};
+}
 
         // do {
         // const humanSelection = humanChoice;
